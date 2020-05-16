0334963048443cffaf00e5976077b8f45a3c9e22
Alexander Wiegel <alexander.wiegel@rwth-aachen.de>
2020-05-16 17:08:49 +0200

    Remove openFile menu option and add open in blender

--- Alpha/Blender/Blender.py
+++ Alpha/Blender/Blender.py
@@ -1,10 +1,15 @@
+import subprocess
+
 from UM.Extension import Extension
+from UM.Application import Application
 from UM.i18n import i18nCatalog
 from UM.Logger import Logger
 from UM.Message import Message
 
 from PyQt5.QtCore import QTimer, QUrl
 from PyQt5.QtGui import QDesktopServices
+
+from . import BLENDReader
 
 i18n_catalog = i18nCatalog('uranium')
 
@@ -13,19 +18,16 @@ class Blender(Extension):
     def __init__(self):
         super().__init__()
         self.setMenuName(i18n_catalog.i18nc('@item:inmenu', 'Blender'))
-        self.addMenuItem(i18n_catalog.i18nc('@item:inmenu', 'Select Blender File'), self.openFile)
         self.addMenuItem(i18n_catalog.i18nc('@item:inmenu', 'Scale Size'), self.scaleSize)
         self.addMenuItem(i18n_catalog.i18nc('@item:inmenu', 'Check Waterproof'), self.checkWaterproof)
         self.addMenuItem(i18n_catalog.i18nc('@item:inmenu', 'Reload Object'), self.reloadFile)
 
+        BLENDReader.global_path = None
+        BLENDReader.blender_path = None
+
     def getMessage(self, title, text):
         message = Message(text=i18n_catalog.i18nc('@info', text), title=i18n_catalog.i18nc('@info:title', title))
         return message
-
-    def openFile(self):
-        Logger.log('i', 'Select Blender File is currently under development.')
-        message = self.getMessage('Work in Progress', 'Select Blender File is not implemented yet.')
-        message.show()
 
     def scaleSize(self):
         Logger.log('i', 'Scale Size is currently under development.')
@@ -55,6 +57,11 @@ class Blender(Extension):
         this function cannot be there.
         '''
         if action == 'Open in Blender':
-            QDesktopServices.openUrl(QUrl('https://www.blender.org/download/'))
+            if BLENDReader.blender_path is None:
+                QDesktopServices.openUrl(QUrl('https://www.blender.org/download/'))
+            elif BLENDReader.global_path is None:
+                subprocess.Popen(BLENDReader.blender_path, shell = True)
+            else:
+                subprocess.Popen((BLENDReader.blender_path, BLENDReader.global_path), shell = True)
         elif action == 'Ignore':
             message.hide()
